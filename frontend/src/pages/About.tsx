62623edad96438e62392a64438434f8a096b9bce
Nugroho Adi Susanto <you@example.com>
2025-03-10 12:52:08 +0700

    add microservices and routing

---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      <h1>About Anycademy</h1>
+      <p>Learn more about our platform</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      <h1>Welcome to Anycademy</h1>
+      <p>Your learning journey starts here</p>
+    </div>
+  );
+};
+
+export default Home;
